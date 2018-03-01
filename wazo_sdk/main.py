89352010f194948ed3276b0803b469d91b9e1269
Pascal Cadotte Michaud <pcm@wazo.io>
2018-03-01 11:45:06 -0500

    add the dev directory and remote host to the cli arguments

--- wazo_sdk/main.py
+++ wazo_sdk/main.py
@@ -17,7 +17,16 @@ class WDK(App):
             command_manager=CommandManager('wazo_sdk.commands'),
             version='0.0.1',
         )
-        self._mounter = Mounter(self.LOG)
+
+    def build_option_parser(self, *args, **kwargs):
+        parser = super().build_option_parser(*args, **kwargs)
+        parser.add_argument('--hostname', help='The remove host when Wazo is installed')
+        parser.add_argument('--dev-dir', help='Where the local source code is')
+        return parser
+
+    def initialize_app(self, argv):
+        self.LOG.debug('options=%s', self.options)
+        self._mounter = Mounter(self.LOG, self.options.hostname, self.options.dev_dir)
 
     def prepare_to_run_command(self, cmd):
         cmd.mounter = self._mounter
--- wazo_sdk/mount.py
+++ wazo_sdk/mount.py
@@ -1,11 +1,22 @@
 # Copyright 2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+import os
+
 
 class Mounter:
 
-    def __init__(self, logger):
+    def __init__(self, logger, hostname, local_dir):
         self.logger = logger
+        self._hostname = hostname
+        self._local_dir = local_dir
 
     def mount(self, repo_name):
-        self.logger.info('mounting %s', repo_name)
+        if not self._hostname:
+            raise Exception('The remote hostname is required to mount directories')
+
+        if not self._local_dir:
+            raise Exception('The local source directory is required to mount directories')
+
+        repo_path = os.path.join(self._local_dir, repo_name)
+        self.logger.info('mounting %s on %s', repo_path, self._hostname)
