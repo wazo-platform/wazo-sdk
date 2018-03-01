ec5f985e5e729734d69caad1d98edc364d24d152
Pascal Cadotte Michaud <pcm@wazo.io>
2018-03-01 13:27:07 -0500

    mount: sync repo using lsyncd

--- wazo_sdk/mount.py
+++ wazo_sdk/mount.py
@@ -2,6 +2,9 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import os
+import subprocess
+
+REPO_PREFIX = ['', 'wazo-', 'xivo-']
 
 
 class Mounter:
@@ -10,6 +13,8 @@ class Mounter:
         self.logger = logger
         self._hostname = hostname
         self._local_dir = local_dir
+        # TODO: make this configurable
+        self._remote_dir = '/root/dev/wazo'
 
     def mount(self, repo_name):
         if not self._hostname:
@@ -18,5 +23,26 @@ class Mounter:
         if not self._local_dir:
             raise Exception('The local source directory is required to mount directories')
 
-        repo_path = os.path.join(self._local_dir, repo_name)
-        self.logger.info('mounting %s on %s', repo_path, self._hostname)
+        complete_repo_name = self._find_complete_repo_name(repo_name)
+
+        self._start_sync(complete_repo_name)
+
+    def _start_sync(self, repo_name):
+        local_path = os.path.join(self._local_dir, repo_name)
+        remote_path = os.path.join(self._remote_dir, repo_name)
+
+        lsync_command = ['lsyncd', '-delay', '1', '-rsyncssh', local_path, self._hostname, remote_path]
+
+        self.logger.debug('%s', ' '.join(lsync_command))
+        ret = subprocess.call(lsync_command)
+        if ret:
+            self.logger.info('%s failed %s', ' '.join(lsync_command), ret)
+
+    def _find_complete_repo_name(self, repo_name):
+        for prefix in REPO_PREFIX:
+            basename = '{}{}'.format(prefix, repo_name)
+            path = os.path.join(self._local_dir, basename)
+            if os.path.exists(path):
+                return basename
+
+        raise Exception('No such repo {}'.format(repo_name))
