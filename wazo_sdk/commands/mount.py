81a44990d149011fda68a24dfb6be4235ea712d3
Francois Blackburn <fblackburn@wazo.community>
2018-03-30 11:44:41 -0400

    add restart option to mount command

--- wazo_sdk/commands/mount.py
+++ wazo_sdk/commands/mount.py
@@ -10,12 +10,15 @@ class Mount(Command):
     def get_parser(self, *args, **kwargs):
         parser = super().get_parser(*args, **kwargs)
         parser.add_argument('--list', action='store_true', help='list mounted repositories')
+        parser.add_argument('--restart', '-r',  action='store_true', help='restart mounted repositories')
         parser.add_argument('repos', nargs='*', default=[], help='a list repos to mount')
         return parser
 
     def take_action(self, parsed_args):
         for repo in parsed_args.repos:
             self.mounter.mount(repo)
+            if parsed_args.restart:
+                self.service.restart(repo)
 
         if parsed_args.list:
             mounted_repos = self.mounter.list_()
